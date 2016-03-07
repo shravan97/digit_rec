8318f8558fe9384046b2d01f224694498ca719e7
ajayrfhp <ajayrfhp1710@gmail.com>
2016-03-07 13:10:43 +0530

    added grid search

--- digit_rec_svm.py
+++ digit_rec_svm.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_digits
 from sklearn import svm,metrics
 from sklearn.cross_validation import train_test_split,cross_val_score
+from sklearn.grid_search import GridSearchCV
 import numpy as np
 
 digits = load_digits()
@@ -17,17 +18,17 @@ scores_across_gammas = []
 
 #Use expected and predicted for final test only. Evaluate Model accuracy, parameter accuracy with cross validation score.
 
-gamma_values = np.linspace(0.001,1,100)
-for gamma_value in gamma_values:
-	classifier = svm.SVC(gamma=gamma_value,kernel = 'rbf')
-	classifier.fit(X_train,Y_train)
-	cross_validation_scores = cross_val_score(classifier,X_train,Y_train.reshape(Y_train.shape[0],),cv = 5)
-	mean_cross_validation_score = np.mean(cross_validation_scores)
-	scores_across_gammas.append(mean_cross_validation_score)
-	print gamma_value,mean_cross_validation_score
+parameters = [{'kernel':['rbf'],'gamma':[1e-2,1e-3,1e-4]},
+			 {'kernel':['linear'],'gamma':[1e-2,1e-3,1e-4],'degree':[1,2,3,4,5,6,7,8,9,10]}]
 
-scores_across_gammas = np.array(scores_across_gammas)
-print scores_across_gammas.shape
+classifier = GridSearchCV(svm.SVC(),parameters)
+classifier.fit(X_train,Y_train.reshape(Y_train.shape[0],))
+cross_validation_scores = cross_val_score(classifier,X_train,Y_train.reshape(Y_train.shape[0],),cv = 5)
+print cross_validation_scores
+
+
+
+
 
 
 
