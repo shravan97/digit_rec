c259dec5da50e3104d4af9dfc7583d54991413ba
ajayrfhp <ajayrfhp1710@gmail.com>
2016-03-17 00:12:31 +0530

    simple structure, initial commit
    first structure
    
    first structure
    
    removed build

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='license_plate_recognition',
+      version='0.1',
+      description='Detect license plate number of a car',
+      url='https://github.com/ajayrfhp/license_plate_recognition',
+      author='ajayrfhp',
+      author_email='ajayrfhp1710@gmail.com',
+      license='MIT',
+      packages=['license_plate_recognition'],
+      zip_safe=False)
