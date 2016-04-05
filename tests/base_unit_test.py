c0cdd4502559547e8b30814cd58e71afac6c0cf8
ajayrfhp <ajayrfhp1710@gmail.com>
2016-04-06 01:32:29 +0530

    change to digit_rec

--- tests/base_unit_test.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-import license_plate_recognition
-
-class TestHelloWorld(TestCase):
-	def test_is_string(self):
-		string = license_plate_recognition.hello_world()
-		self.assertTrue(isinstance(string,basestring))
-
