5f29377ef06492706097326c86ac57a7a6b10258
shravan97 <shravanmurali@gmail.com>
2016-04-12 17:02:28 +0530

    added ANN to the models list

--- digit_rec/models.py
+++ digit_rec/models.py
@@ -1,5 +1,6 @@
 from util import *
 import util
+
 def model(model_type):
 	classifier = None
 	if( model_type == 'svm' ):
@@ -7,7 +8,7 @@ def model(model_type):
 	elif( model_type == 'rf'):
 		classifier = RandomForestClassifier()
 	elif( model_type == 'ann' ):
-		classifier = None
+		classifier = Classifier(layers=[Layer("Softmax" , units=1000),Layer("Softmax" , units=10)],learning_rate=0.02,n_iter=1000)
 	return classifier
 
 
